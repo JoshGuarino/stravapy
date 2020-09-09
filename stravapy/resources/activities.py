35b6b774bb91b30503d8ffcd53e8957df60325f9
JoshGuarino <guar0638@fredonia.edu>
2020-09-09 16:33:00 -0400

    activities structure

---
+++ stravapy/resources/activities.py
@@ -0,0 +1,26 @@
+class Activities(object):
+    URL = '/activities'
+
+    def create_activity(request):
+        return
+
+    def get_activity_by_id(request):
+        return
+    
+    def get_comments_by_activity_id(request):
+        return
+
+    def get_kudoers_by_activity_id(request):
+        return
+
+    def get_lap_by_activity_id(request):
+        return
+
+    def get_logged_athlete_activities(request):
+        return
+
+    def get_zones_by_activity_id(request):
+        return
+
+    def update_activity_by_id(request):
+        return
