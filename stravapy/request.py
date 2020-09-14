b7cb404137535f2ccda2fa03b9ba65fc6d4e289d
JoshGuarino <guar0638@fredonia.edu>
2020-09-14 15:30:17 -0400

    code cleanup and activiies param map

--- stravapy/request.py
+++ stravapy/request.py
@@ -17,6 +17,10 @@ class Request(object):
         except requests.exceptions.HTTPError as error:
             raise SystemExit(error)
 
-    def test():
-        print('test2')
-        return 
+    def put(headers, url):
+        try:
+            response = request.get(url=url, headers=headers)
+            if response.status_code == 200:
+                return response
+        except requests.exceptions.HTTPError as error:
+            raise SystemExit(error)
--- stravapy/resources/activities.py
+++ stravapy/resources/activities.py
@@ -2,26 +2,34 @@ from request import Request
 
 class Activities(object):
 
-    def create_activity():
+    def create_activity(name, type, start_date_local, elapsed_time, description=None, distance=None, trainer=None, commute=None):
+        Request.post()
         return
 
-    def get_activity_by_id():
+    def get_activity_by_id(id, include_all_efforts=None):
+        Request.get()
         return
     
-    def get_comments_by_activity_id():
+    def get_comments_by_activity_id(id, page=None, per_page=None):
+        Request.get()
         return
 
-    def get_kudoers_by_activity_id():
+    def get_kudoers_by_activity_id(id, page=None, per_page=None):
+        Request.get()
         return
 
-    def get_lap_by_activity_id():
+    def get_lap_by_activity_id(id):
+        Request.get()
         return
 
-    def get_logged_athlete_activities():
+    def get_logged_athlete_activities(before=None, after=None, page=None, per_page=None):
+        Request.get()
         return
 
-    def get_zones_by_activity_id():
+    def get_zones_by_activity_id(id):
+        Request.get()
         return
 
-    def update_activity_by_id():
+    def update_activity_by_id(id):
+        Request.put()
         return
