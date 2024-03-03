54c3e71aa7f73dfe0dab4b680781753281dca50d
Josh Guarino <jguarino722@gmail.com>
2024-03-03 16:57:16 -0500

    add pagination to clubs resource

--- stravapy/resources/clubs.py
+++ stravapy/resources/clubs.py
@@ -5,18 +5,18 @@ class Clubs:
         self.url = url
         self.headers = headers
 
-    def get_club_activities_by_id(self, id: int):
-        r = Request.get(f'{self.url}/{id}/activities', self.headers)
+    def get_club_activities_by_id(self, id: int, page: int = 1, per_page: int = 30):
+        r = Request.get(f'{self.url}/{id}/activities?page={page}&per_page={per_page}', self.headers)
         return r.json()
 
-    def get_club_admins_by_id(self, id: int):
-        r = Request.get(f'{self.url}/{id}/admins', self.headers)
+    def get_club_admins_by_id(self, id: int, page: int = 1, per_page: int = 30):
+        r = Request.get(f'{self.url}/{id}/admins?page={page}&per_page={per_page}', self.headers)
         return r.json()
 
     def get_club_by_id(self, id: int):
         r = Request.get(f'{self.url}/{id}', self.headers)
         return r.json()
 
-    def get_club_members_by_id(self, id: int):
-        r = Request.get(f'{self.url}/{id}/members', self.headers)
+    def get_club_members_by_id(self, id: int, page: int = 1, per_page: int = 30):
+        r = Request.get(f'{self.url}/{id}/members?page={page}&per_page={per_page}', self.headers)
         return r.json()
